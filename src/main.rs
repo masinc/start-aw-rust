8584a9fb90d99cdf83e7f1f5d85d1c7e5d4ff6a1
masinc <masinc000@gmail.com>
2020-07-12 18:54:13 +0900

    async hello

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use std::io;
+
+use actix_web::{web, App, HttpRequest, HttpServer, Responder, Result};
+
+async fn hello(req: HttpRequest) -> Result<impl Responder> {
+    let to = req.match_info().get("name").unwrap_or("World");
+    Ok(format!("Hello {}!", to))
+}
+
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(web::resource("/").to(hello))
+            .service(web::resource("/{name}").to(hello))
+    })
+    .bind("localhost:3000")?
+    .run()
+    .await
+}
