b0e7f8c088aa287350a8460d20084132fe12f304
masinc <masinc000@gmail.com>
2020-07-18 16:05:52 +0900

    response to static files

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,14 @@
 use std::io;
 
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
-
-async fn hello(req: HttpRequest) -> impl Responder {
-    let to = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", to)
-}
+use actix_files::Files;
+use actix_web::{App, HttpServer};
 
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(web::resource("/").to(hello))
-            .service(web::resource("/{name}").to(hello))
-    })
-    .bind("localhost:3000")
-    .expect("Can not bind to port 3000")
-    .run()
-    .await
+    HttpServer::new(||
+        App::new().service(Files::new("/", "./").show_files_listing()))
+        .bind("localhost:3000")
+        .expect("Can not bind to port 3000")
+        .run()
+        .await
 }
