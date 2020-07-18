8cabe63ed9d1a310561563932f1738a5a541dbc2
masinc <masinc000@gmail.com>
2020-07-18 20:37:23 +0900

    response to template

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,40 @@
+use actix_web::{error, web, App, HttpResponse, HttpServer, Result};
+use serde_derive::*;
 use std::io;
+use tera::{Context, Tera};
 
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+struct AppState {
+    template: Tera,
+}
 
-async fn hello(req: HttpRequest) -> impl Responder {
-    let to = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", to)
+#[derive(Deserialize)]
+struct HelloPath {
+    name: String,
+}
+
+async fn hello_template(
+    app: web::Data<AppState>,
+    path: web::Path<HelloPath>,
+) -> Result<HttpResponse> {
+    let mut context = Context::new();
+    context.insert("name", &path.name);
+
+    let body = app
+        .template
+        .render("index.html.tera", &context)
+        .map_err(|e| error::ErrorInternalServerError(format!("{}", e)))?;
+    Ok(HttpResponse::Ok().body(body))
 }
 
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
     HttpServer::new(|| {
+        let app = AppState {
+            template: Tera::new("templates/**/*").unwrap(),
+        };
         App::new()
-            .service(web::resource("/").to(hello))
-            .service(web::resource("/{name}").to(hello))
+            .data(app)
+            .route("/{name}", web::get().to(hello_template))
     })
     .bind("localhost:3000")
     .expect("Can not bind to port 3000")
